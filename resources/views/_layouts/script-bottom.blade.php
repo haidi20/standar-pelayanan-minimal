c3dc88ed4a12f57ecd78c5a4a2856a00d5245b6b
haidinata <haidinurhadinata@gmail.com>
2018-10-27 21:13:49 +0700

    update-kuisioner-heroku

--- resources/views/_layouts/script-bottom.blade.php
+++ resources/views/_layouts/script-bottom.blade.php
@@ -1,2 +1,3 @@
 <script src="{{mix('js/app.js')}}"></script>
 <script src="{{asset('js/jquery-session/jquery.session.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery.session@1.0.0/jquery.session.min.js"></script>
